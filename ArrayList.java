dda5ea21819c4c9efdc12bc4e8c544a235820479
xxxxBERSxxxx <151732361+xxxxBERSxxxx@users.noreply.github.com>
2025-03-23 21:59:37 +0300

    Update ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -2,60 +2,60 @@ import java.util.Arrays;
 
 
 public class ArrayList {
-    private static String[] list = new String[10]; // Исходный массив
-    private static int size = 0; // Текущий размер
+    private static String[] list = new String[10]; 
+    private static int size = 0; 
 
     public static void add(String element) {
-        // Если массив заполнен, увеличиваем его размер
+      
         if (size == list.length) {
             String[] newList = new String[list.length * 2];
             newList = list.clone();
             list = newList;
         }
-        list[size++] = element; // Добавляем элемент
+        list[size++] = element; 
     }
 
     public static boolean remove(String element) {
-        // Поиск элемента в массиве
+        
         for (int i = 0; i < size; i++) {
             if ((element == null && list[i] == null) ||
                     (element != null && element.equals(list[i]))) {
 
-                // Сдвиг элементов влево
+           
                 for (int j = i; j < size - 1; j++) {
                     list[j] = list[j + 1];
                 }
 
-                list[--size] = null; // Очистка последней позиции
-                return true; // Элемент удален
+                list[--size] = null; 
+                return true; 
             }
         }
-        return false; // Элемент не найден
+        return false; 
     }
 
     public static String set(int index, String newElement) {
-        String oldElement = list[index]; // Сохраняем старый элемент
-        list[index] = newElement;         // Заменяем на новый
-        return oldElement;                // Возвращаем старый элемент
+        String oldElement = list[index]; 
+        list[index] = newElement;        
+        return oldElement;               
     }
 
     public static void clear() {
-        // Обнуляем все элементы до текущего размера
+        
         for (int i = 0; i < size; i++) {
             list[i] = null;
         }
-        size = 0; // Сбрасываем размер
+        size = 0; 
     }
 
 
     public static void main(String[] args) {
-        add("Apple"); // Добавляем элементы
+        add("Apple"); 
         add("Banana");
         add("Cherry");
         add("Cherry");
 
         System.out.println(Arrays.toString(list));
-        System.out.println("Size: " + size); // Size: 2
+        System.out.println("Size: " + size); 
 
         remove("Banana");
         System.out.println("После удаления: " + Arrays.toString(list));
